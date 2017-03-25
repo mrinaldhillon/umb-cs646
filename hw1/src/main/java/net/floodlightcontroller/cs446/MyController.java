763fc42a428db328f8414fabdbd13aa35262cdaf
mrinaldhillon <mrinaldhillon@gmail.com>
2017-03-25 03:48:03 -0400

    * H1 * Added flow control of IP packets from *   h1->1-sw1-2/3->..->1/2-sw4-3->hw4. * Issue: It seems that rules from the default floodlight controller takes *   precedence in defining the end to end route. * That is why even though dump flow shows MyController rule *   InPort1-Sw1-OutPort 3 sticking, unexpected entries are also seen. * This behavior reflects on Wireshark as well, where request packets are *   flowing through Sw2 as well Sw3 at the same time. * Following Flow is not as expected. *   Flow Expected :       InPort1-Sw1-OutPort3->InPort1-Sw3-OutPort2->InPort2-Sw4-OutPort3 * Flow Actual: *     InPort1-Sw1-OutPort2->InPort1-Sw2-OutPort2->InPort1-Sw4-OutPort3 * Resolution 3 Steps: * 1. #isCallbackOrderingPostreq: Make floodlight defualt-contoller load for same flow after *   MyController. * 2. #receive: Set Command.STOP for flow handled by MyController. * 3. Handled all Ip flow from h1 to h4 via every switch. * - Combination of above two steps completely disables the *   defualt-controlled for flow beng handeled by MyController. * - Since first step we are setting up default controller to be called *   only once the packet has been handled by MyController. * - However after handling the packet Command.STOP instructs floodlight *   to not call any other controller, hence disables defualt controller for *   the intersting flow. * - 3rd Step makes sure there is flow rule on each switch for h1->..>h4. * Result: * If MyController correctly handles ICMP Request flow rules for all *   switches. Flow is as Expected : *     InPort1-Sw1-OutPort3->InPort1-Sw3-OutPort2->InPort2-Sw4-OutPort3. * This also reflects on Wireshark. * Todo: * - Research constrains on protocol and attributes of the flow to be *   handled. Figure ot the minimum match conditions that could suffice. * - Make implementation more compact.

--- hw1/src/main/java/net/floodlightcontroller/cs446/MyController.java
+++ hw1/src/main/java/net/floodlightcontroller/cs446/MyController.java
@@ -61,8 +61,21 @@ public class MyController implements IOFMessageListener, IFloodlightModule {
   protected OFMessageDamper messageDamper;
   private int OFMESSAGE_DAMPER_CAPACITY = 10000;
   private int OFMESSAGE_DAMPER_TIMEOUT = 250; // ms
-  private int prev_oport = -1;
   public static final int FORWARDING_APP_ID = 446;
+
+  private static final int MODFLOW_IDLE_TIMEOUT = 5;
+  private static final int MODFLOW_HARD_TIMEOUT = 10;
+  private static final int MODFLOW_PRIORITY = 1000;
+
+  private int last_used_oport = -1;
+  private static final String SW1_ID = "00:00:00:00:00:00:00:01";
+  private static final String SW2_ID = "00:00:00:00:00:00:00:02";
+  private static final String SW3_ID = "00:00:00:00:00:00:00:03";
+  private static final String SW4_ID = "00:00:00:00:00:00:00:04";
+
+  private static final String H1_IP = "10.0.0.1";
+  private static final String H4_IP = "10.0.0.2";
+
   static {
     AppCookie.registerApp(FORWARDING_APP_ID, "forwarding");
   }
@@ -82,7 +95,7 @@ public class MyController implements IOFMessageListener, IFloodlightModule {
   @Override
   public boolean isCallbackOrderingPostreq(OFType type, String name) {
     // TODO Auto-generated method stub
-    return false;
+    return true;
   }
 
   @Override
@@ -125,44 +138,139 @@ public class MyController implements IOFMessageListener, IFloodlightModule {
 
   }
 
+  private Match createMyMatchFromPacket(IOFSwitch sw, OFPort inPort, EthType ethType, 
+      MacAddress srcMac, MacAddress destMac, IPv4Address srcIp, IPv4Address destIp) {
+    Match.Builder mb = sw.getOFFactory().buildMatch()
+      .setExact(MatchField.IN_PORT, inPort)
+      .setExact(MatchField.ETH_TYPE, ethType)
+      .setExact(MatchField.ETH_SRC, srcMac)
+      .setExact(MatchField.ETH_DST, destMac)
+      .setExact(MatchField.IPV4_SRC, srcIp)
+      .setExact(MatchField.IPV4_DST, destIp);
+    return mb.build();
+  }
+
+  private void writeMyFlowMod(IOFSwitch sw, Match match, OFPort outPort) {
+    OFFactory myFactory=sw.getOFFactory();
+    OFFlowMod.Builder fmb;
+    OFActions actions = myFactory.actions();
+
+    ArrayList<OFAction> actionList = new ArrayList<OFAction>();
+    OFActionOutput output = actions.buildOutput()
+      .setPort(outPort)
+      .setMaxLen(0xFFffFFff)
+      .build();
+    actionList.add(output);
+
+    fmb=myFactory.buildFlowAdd()
+      .setMatch(match)
+      .setCookie(cookie)
+      .setIdleTimeout(MODFLOW_IDLE_TIMEOUT)
+      .setHardTimeout(MODFLOW_HARD_TIMEOUT)
+      .setPriority(MODFLOW_PRIORITY)
+      .setBufferId(OFBufferId.NO_BUFFER)
+      .setOutPort(outPort)
+      .setActions(actionList);
+
+    messageDamper.write(sw, fmb.build());
+  }
+
+  private void handlePacketInFlowForSw1ToSw4(IOFSwitch sw, OFPacketIn pi, FloodlightContext cntx) {
+    int oport = -1;
+    OFPort inPort = (pi.getVersion().compareTo(OFVersion.OF_12) < 0 
+        ? pi.getInPort() : pi.getMatch().get(MatchField.IN_PORT));
+
+    Ethernet eth = IFloodlightProviderService.bcStore.get(cntx, 
+        IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
+    MacAddress srcMac = eth.getSourceMACAddress();
+    MacAddress destMac = eth.getDestinationMACAddress();
+    IPv4 pkt = (IPv4) eth.getPayload();
+    IPv4Address srcIp = pkt.getSourceAddress();
+    IPv4Address destIp = pkt.getDestinationAddress();
+    String srcIpString = pkt.getSourceAddress().toString();
+    String destIpString = pkt.getDestinationAddress().toString();
+
+    if (!srcIpString.equals(H1_IP) || !destIpString.equals(H4_IP)) {
+      throw new UnsupportedOperationException("MyController does not handle flow from"
+          + srcIpString + " to " + destIpString);
+    }
+
+    String swId = sw.getId().toString();
+    switch(swId) {
+      case SW1_ID:
+               if (1 != inPort.getPortNumber()) {
+                 throw new UnsupportedOperationException("MyController does not handle flow of Switch Id:" 
+                     +  sw.getId().toString() + "InPort:" + inPort.getPortNumber());
+               }
+               oport = 2 == last_used_oport ? 3 : 2;
+               logger.info("Last Used OutPort:{}", Integer.toString(last_used_oport));         
+               // Set previous port
+               last_used_oport = oport;
+               break;
+      case SW2_ID:
+      case SW3_ID:
+               if (1 != inPort.getPortNumber()) {
+                 throw new UnsupportedOperationException("MyController does not handle flow of Switch Id:" 
+                     +  sw.getId().toString() + "InPort:" + inPort.getPortNumber());
+               }
+               oport = 2;
+               break;
+      case SW4_ID:
+               if (1 != inPort.getPortNumber() && 2 != inPort.getPortNumber()) {
+                 throw new UnsupportedOperationException("MyController does not handle flow of Switch Id:" 
+                     +  sw.getId().toString() + "InPort:" + inPort.getPortNumber());
+               }
+               oport = 3;
+               break;
+      default:
+               throw new UnsupportedOperationException("MyController does not handle flow of Switch Id:" 
+                   +  sw.getId().toString() + "InPort:" + inPort.getPortNumber());
+    }
+
+    logger.info("Switch Id:{}", swId);         
+    logger.info("InPort:{}, OutPort:{}", Integer.toString(inPort.getPortNumber()), Integer.toString(oport));
+    Match m = createMyMatchFromPacket(sw, inPort, EthType.IPv4, 
+        srcMac, destMac, srcIp, destIp);
+    writeMyFlowMod(sw, m, OFPort.of(oport));
+  }
+
   @Override
   public net.floodlightcontroller.core.IListener.Command receive(IOFSwitch sw, OFMessage msg,
       FloodlightContext cntx) {
-    // TODO Auto-generated method stub
-    switch(msg.getType()){
-      case PACKET_IN:
-        Ethernet eth =
-          IFloodlightProviderService.bcStore.get(cntx,
-              IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
-        MacAddress srcMac = eth.getSourceMACAddress();
-
-        if(eth.getEtherType()==EthType.IPv4){
-          IPv4 pkt = (IPv4) eth.getPayload();
-          if(pkt.getProtocol()== IpProtocol.ICMP){
-            logger.info("Src MAC:{}, Src IP:{}",
-                srcMac.toString(),
-                pkt.getSourceAddress().toString());
-            logger.info("ip port:{}",
-                Integer.toString( ((OFPacketIn)msg).getInPort().getPortNumber() ));
-            if (sw.getId().toString().equals("00:00:00:00:00:00:00:01") 
-                && pkt.getSourceAddress().toString().equals("10.0.0.1")
-                && 1 == ((OFPacketIn)msg).getInPort().getPortNumber()) {
-              addMyFlowForSwitch1PortTypeInPortNum1SrcH1(sw, (OFPacketIn)msg, cntx);
-            } else {
+    Command cmd = Command.CONTINUE;
+    try {
+      // TODO Auto-generated method stub
+      switch(msg.getType()){
+        case PACKET_IN:
+          Ethernet eth =
+            IFloodlightProviderService.bcStore.get(cntx,
+                IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
+          MacAddress srcMac = eth.getSourceMACAddress();
+          if(eth.getEtherType()==EthType.IPv4){
+            IPv4 pkt = (IPv4) eth.getPayload();
+            if(pkt.getProtocol()== IpProtocol.ICMP){
+              logger.info("Src MAC:{}, Src IP:{}",
+                  srcMac.toString(),
+                  pkt.getSourceAddress().toString());
+              logger.info("ip port:{}",
+                  Integer.toString( ((OFPacketIn)msg).getInPort().getPortNumber() ));
+              handlePacketInFlowForSw1ToSw4(sw, (OFPacketIn)msg, cntx);  
               // addMyFlow(sw);
+              cmd = Command.STOP;
             }
-            // addMyFlow(sw);
-            //return Command.STOP;
           }
-        }
-        break;
-      default:
-        break;
+          break;
+        default:
+          break;
+      }
+    } catch (UnsupportedOperationException e) {
+      //logger.info(e.getMessage());
+    } finally {
+      return cmd;
     }
-    return Command.CONTINUE;
   }
 
-  void addMyFlow(IOFSwitch sw){
+  private void addMyFlow(IOFSwitch sw){
     OFFlowMod.Builder fmb;
     OFFactory myFactory=sw.getOFFactory();
     fmb=myFactory.buildFlowAdd();
@@ -191,62 +299,6 @@ public class MyController implements IOFMessageListener, IFloodlightModule {
       .setCookie(cookie)
       .setPriority(1)
       .setMatch(myMatch);
-
-    FlowModUtils.setActions(fmb, actionList, sw);
-    messageDamper.write(sw, fmb.build());
-  }
-
-  // Based on LearningSwitch 
-  private Match createMatchFromPacket(IOFSwitch sw, FloodlightContext cntx, OFPort inPort, EthType ethType) {
-    Ethernet eth = IFloodlightProviderService.bcStore.get(cntx, IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
-    MacAddress srcMac = eth.getSourceMACAddress();
-    MacAddress dstMac = eth.getDestinationMACAddress();
-    IPv4 pkt = (IPv4) eth.getPayload();
-
-    Match.Builder mb = sw.getOFFactory().buildMatch();
-    mb.setExact(MatchField.IN_PORT, inPort)
-      .setExact(MatchField.ETH_TYPE, ethType)
-      .setExact(MatchField.ETH_SRC, srcMac)
-      .setExact(MatchField.ETH_DST, dstMac)
-      .setExact(MatchField.IPV4_SRC, IPv4Address.of(pkt.getSourceAddress().toString()))
-      .setExact(MatchField.IPV4_DST, IPv4Address.of(pkt.getDestinationAddress().toString()));
-    //    .setExact(MatchField.IP_PROTO, IpProtocol.TCP);
-    return mb.build();
-  }
-
-  void addMyFlowForSwitch1PortTypeInPortNum1SrcH1(IOFSwitch sw, OFPacketIn pi, 
-      FloodlightContext cntx) {
-
-    OFFactory myFactory = sw.getOFFactory();
-    OFFlowMod.Builder fmb = myFactory.buildFlowAdd();
-    OFPort inPort = (pi.getVersion().compareTo(OFVersion.OF_12) < 0 ? pi.getInPort() : 
-        pi.getMatch().get(MatchField.IN_PORT));
-    Match m = createMatchFromPacket(sw, cntx, inPort, EthType.IPv4);
-    ArrayList<OFAction> actionList = new ArrayList<OFAction>();
-    OFActions actions = myFactory.actions();
-
-    // Chose alternate outport
-    int oport = 2 == prev_oport ? 3 : 2;
-    System.out.println("Switch Id: " + sw.getId());
-    System.out.println("Previous OutPort: " + prev_oport);
-    System.out.println("Selected OutPort: " + oport);
-    // Set previous port
-    prev_oport = oport;
-
-    OFActionOutput output = actions.buildOutput()
-      .setMaxLen(0xFFffFFff)
-      .setPort(OFPort.of(oport))
-      .build();
-    actionList.add(output);
-
-    fmb
-      .setIdleTimeout(5)
-      .setHardTimeout(5)
-      .setBufferId(OFBufferId.NO_BUFFER)
-      .setCookie(cookie)
-      .setPriority(100)
-      .setOutPort(OFPort.of(oport))
-      .setMatch(m);
 
     FlowModUtils.setActions(fmb, actionList, sw);
     messageDamper.write(sw, fmb.build());
